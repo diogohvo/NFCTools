d1f6723304cf348398aa418ca858f7bb1375e600
diogohvo <diogohvo@ua.pt>
2024-03-18 17:07:36 +0000

    UI draw-up completed

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,11 @@ class HomepageNav extends StatefulWidget {
 class _HomepageNav extends State<HomepageNav> {
   int currentPageIndex = 0;
   final _pageViewController = PageController();
+  Widget readPage = BeforeRead(theme: ThemeData.dark(useMaterial3: true));
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
-    Widget readPage = BeforeRead(theme: theme);
 
     return Scaffold(
       appBar: AppBar(
@@ -261,19 +261,58 @@ class AfterRead extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(Icons.nfc, size: 40),
-          const SizedBox(height: 22.0),
-          const Icon(Icons.nfc, size: 40),
-          const SizedBox(height: 22.0),
-          Text(
-            'Dispositivo lido',
-            style: theme.textTheme.titleLarge,
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.memory),
+              title: Text('Tipo de tag:'),
+              subtitle: Text('NXP - NTAG213'),
+            ),
           ),
-          const SizedBox(height: 90),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.confirmation_number),
+              title: Text('Serial Number'),
+              subtitle: Text('04:61:E3:AA:07:AF:81'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.key),
+              title: Text('Protegido por senha'),
+              subtitle: Text('Não'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.storage),
+              title: Text('Informação da Memória'),
+              subtitle: Text('180 bytes: 45 páginas (4 bytes cada)'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.storage_rounded),
+              title: Text('Tamanho'),
+              subtitle: Text('4 / 137 bytes'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.replay_outlined),
+              title: Text('Gravável'),
+              subtitle: Text('Sim'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.content_paste_search),
+              title: Text('Conteúdo'),
+              subtitle: Text('Vazio'),
+            ),
+          ),
         ],
       ),
     );
